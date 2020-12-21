12e518ae42db2af3164b25987dc0f8d42bf643df
ShyroVO <bosquet.laurie@outlook.com>
2020-12-21 12:59:25 +0100

    0.1 /

---
+++ app.js
@@ -0,0 +1,11 @@
+let tab = ["param1", "param2", "param3"];
+let div = document.getElementById('monDiv');
+
+function paramVar (){
+    return div.innerHTML = "Bonjour " + tab;
+}
+
+paramVar();
+
+
+
